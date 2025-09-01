8d5b333883d6ae3cd486fa6172b6977035b178ee
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2025-09-01 18:19:19 -0300

    finalizado 2

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 // utils/api.js
 
-export const API_BASE_URL = 'https://localhost:3000/api';
+export const API_BASE_URL = 'https://n8n-cadastrofuncionario-api.r954jc.easypanel.host/api';
 //teste
 /**
  * Obtém o token de autenticação do localStorage.
